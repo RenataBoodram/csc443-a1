8d64b63ed6dccd79c1eef44e029c4ffce3508ec9
RenataBoodram <boodram8@cdf.toronto.edu>
2017-01-26 00:27:29 -0500

    Super preliminary script to run experiments (nothing done).

---
+++ run_experiments.py
@@ -0,0 +1,8 @@
+from subprocess import call
+
+KB = 1024
+MB = 1024 * 1024
+sizes = [ 512, 1 * KB, 4 * KB, 8 * KB, 16 * KB, 32 * KB, 1 * MB, 2 * MB, 4 * MB ]
+
+# 2.2 write_blocks_seq and write_lines
+#call(["./exec, "args", "to", "exec"])
